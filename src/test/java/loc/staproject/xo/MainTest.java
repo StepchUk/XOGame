6b9d528849f32990d6b54483d48d3956021d90b2
StepchUk <stepchyk@gmail.com>
2019-04-05 10:21:41 +0300

    Project initialization

---
+++ src/test/java/loc/staproject/xo/MainTest.java
@@ -0,0 +1,13 @@
+package loc.staproject.xo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void mainTest() {
+        assertTrue(true);
+    }
+}
