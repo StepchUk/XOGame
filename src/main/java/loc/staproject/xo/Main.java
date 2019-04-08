ea8e4e907754eb4778a89c180c3d5c134eef3a52
StepchUk <stepchyk@gmail.com>
2019-04-08 18:02:28 +0300

    CLI v1

--- src/main/java/loc/staproject/xo/Main.java
+++ src/main/java/loc/staproject/xo/Main.java
@@ -1,7 +1,26 @@
 package loc.staproject.xo;
 
+import loc.staproject.xo.model.Field;
+import loc.staproject.xo.model.Figure;
+import loc.staproject.xo.model.Game;
+import loc.staproject.xo.model.Player;
+import loc.staproject.xo.view.ConsoleView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start project");
+        final String name1 = "Alex";
+        final String name2 = "Lexa";
+
+        final Player[] players = new Player[2];
+        players[0] = new Player(name1, Figure.X);
+        players[1] = new Player(name2, Figure.O);
+
+        final Game gameXO = new Game(players, new Field(3), "XO");
+
+        final ConsoleView consoleView = new ConsoleView();
+        consoleView.show(gameXO);
+        while(consoleView.move(gameXO)) {
+            consoleView.show(gameXO);
+        }
     }
 }
